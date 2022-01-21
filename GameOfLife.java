4506e0374eddf2ff06a41e565b7933554e46eaf3
gizmo87898 <coilairconditioner@gmail.com>
2022-01-21 14:01:01 -0500

    idk

--- GameOfLife.java
+++ GameOfLife.java
@@ -2,24 +2,32 @@ import java.awt.*;
 import javax.swing.*;
 import java.util.Random;
 
-public class GameOfLife {
+public class GameOfLife extends JFrame {
     public static void main (String[] args) {
-        JFrame frame = new JFrame("FrameDemo");
-        JLabel emptyLabel = new JLabel("Hi hello");
-        boolean[][] grid = new boolean[10][10];
+
+        
+        //Create grid and print it all out
+        Object[][] grid = new Object[10][10];
         grid = createGrid(5, 10, 10);
         for(int x = 0; x < grid.length; x++) {
             for(int y = 0; y < grid[x].length; y++) {
                 System.out.println(grid[x][y]);
             }
         }
+        //JFrame 
+        JLabel emptyLabel = new JLabel("Hi hello");
+        JTable panelGrid = new JTable(grid, new Object[grid.length][grid[0].length]);
+        panelGrid.setTableHeader(null);
+        JFrame frame = new JFrame("FrameDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        frame.getContentPane().add(new JScrollPane(panelGrid), BorderLayout.CENTER);
         frame.pack();
         frame.setVisible(true);
     }
-    public static boolean[][] createGrid(float seed, int xSize, int ySize) {
-        boolean[][] grid = new boolean[xSize][ySize];
+
+    public static Object[][] createGrid(long seed, int xSize, int ySize) {
+        Object[][] grid = new Object[xSize][ySize];
         for(int x = 0; x < grid.length; x++) {
             for(int y = 0; y < grid[x].length; y++) {
                 Random rd = new Random();
